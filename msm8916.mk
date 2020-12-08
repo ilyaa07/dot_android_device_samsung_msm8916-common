f87405ba50a733dcda97969acabca447017b2c39
Bruno Martins <bgcngm@gmail.com>
2020-12-08 22:03:48 +0000

    msm8916-common: Build Health 2.1 HAL
    Change-Id: Ia13d872d4f03dd08ccd63428397b4c7d6810cb9b

--- msm8916.mk
+++ msm8916.mk
@@ -200,8 +200,8 @@ PRODUCT_VENDOR_KERNEL_HEADERS := \
 
 # Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
