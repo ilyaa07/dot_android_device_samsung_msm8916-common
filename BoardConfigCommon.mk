592100f7b4cb4cc9aaa245031c362960d780b5b0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-28 19:04:50 +0100

    msm8916-common: Move all binaries, libs and firmwares to vendor
    * Following Ib78338130b1b608ed98e3f4b2d43db7feb5cf027
    
    Change-Id: I4b7273b20fcec22c90c4589e5257a963a4833d0a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -271,7 +271,7 @@ SELINUX_IGNORE_NEVERALLOWS := true
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /system/lib/libmmjpeg_interface.so|libboringssl-compat.so \
+    /vendor/lib/libmmjpeg_interface.so|libboringssl-compat.so \
     /system/lib/libsec-ril.so|libshim_secril.so \
     /system/lib/libsec-ril-dsds.so|libshim_secril.so \
     /system/lib/hw/camera.vendor.msm8916.so|libcamera_shim.so \
