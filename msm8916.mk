546b332e7a7cec5fb6890da53498b9388c6af94c
Jeferson <jroliveira.oliveira301@gmail.com>
2021-10-14 10:08:19 +0100

    msm8916-common: Build a vendor copy of android.hardware.drm@1.4
    Change-Id: Ibf3ebd8ff2e0dbd0e7632fdc6441d545ed32ea7a

--- msm8916.mk
+++ msm8916.mk
@@ -152,7 +152,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # Encryption
 PRODUCT_PACKAGES += \
