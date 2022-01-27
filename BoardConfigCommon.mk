299c2c9806c743cb831a16ab9cf1a0e9ac7412ec
Gabriele <gabboxl0@gmail.com>
2022-01-27 23:08:51 +0100

    msm8916-common: Set TARGET_USES_64_BIT_BINDER to true
    Change-Id: I10ef0a3816cd4cdd0d170a822914257028c62572

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -27,6 +27,7 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+TARGET_USES_64_BIT_BINDER := true
 
 # Allow overriding commands during build
 BUILD_BROKEN_DUP_RULES := true
