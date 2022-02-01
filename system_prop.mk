caf9f4c51e397f5f24a7045cd0f1271734a31f87
Gabriele <gabboxl0@gmail.com>
2022-02-01 21:26:59 +0100

    msm8916-common: Disable triple buffering * Historically, triple buffering was used to workaround missed VSYNC   in GPU composition usecases on devices with high resolution panels.   As a downside, an additional frame buffer increased input latency.   Nowadays, most GPUs are fast enough to fit the VSYNC period, rendering   triple framebuffers useless.
      From: https://github.com/MOTO-M8916/android_device_motorola_msm8916-common/commit/c630fd8c679208380abc4524590605b083beead2
    
    Change-Id: I03e2ed08aed863ea2a82e9c6271bfd72626d7106

--- system_prop.mk
+++ system_prop.mk
@@ -74,7 +74,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=1 \
     ro.surface_flinger.start_graphics_allocator_service=true
 
 # FM
